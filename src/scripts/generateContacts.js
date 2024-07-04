0d528e98f15d7a921718ab13e0c5d72eca32c6d4
Viktor Oliinyk <kanoute1992@gmail.com>
2024-07-04 19:32:44 +0300

    add hw_node.js_01

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,22 @@
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
+
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    //   console.log(contacts);
+    return contacts;
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
+};
+
+await generateContacts(5);
